782f892fa0a09069c733057489765a287e221157
Torward <76395706+Torward@users.noreply.github.com>
2021-05-19 17:20:27 +1000

    Add files via upload

---
+++ ClientHandler.java
@@ -0,0 +1,93 @@
+package com.lomov.server;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.SocketException;
+
+public class ClientHandler implements Runnable {
+    private final Socket socket;
+
+    public ClientHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try (
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                DataInputStream in = new DataInputStream(socket.getInputStream())
+        ) {
+            while (true) {
+                String command = in.readUTF();
+                if ("upload".equals(command)) {
+                    uploading(out, in);
+                }
+                if ("download".equals(command)) {
+                    downloading(out, in);
+                }
+                if ("exit".equals(command)) {
+                    out.writeUTF("DONE");
+                    disconnected();
+                    System.out.printf("Client %s disconnected correctly\n", socket.getInetAddress());
+
+                    break;
+                }
+                System.out.println(command);
+                out.writeUTF(command);
+            }
+        } catch (SocketException socketException) {
+            System.out.printf("Client %s disconnected\n", socket.getInetAddress());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void downloading(DataOutputStream out, DataInputStream in) throws IOException {
+        try {
+            File file = new File("client/" + in.readUTF()); //read file name
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileOutputStream fos = new FileOutputStream(file);
+            long size = in.readLong();
+            byte[] buffer = new byte[8 * 1024];
+            for (int i = 0; i < (size + (8 * 1024 - 1)) / buffer.length; i++) {
+                int read = in.read(buffer);
+                fos.write(buffer, 0, read);
+            }
+            fos.close();
+            out.writeUTF("OK");
+        }catch (Exception e){
+            out.writeUTF("WRONG");
+        }
+    }
+
+    private void uploading(DataOutputStream out, DataInputStream in) throws IOException {
+        try {
+            File file = new File("server/" + in.readUTF()); //read file name
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileOutputStream fos = new FileOutputStream(file);
+            long size = in.readLong();
+            byte[] buffer = new byte[8 * 1024];
+            for (int i = 0; i < (size + (8 * 1024 - 1)) / buffer.length; i++) {
+                int read = in.read(buffer);
+                fos.write(buffer, 0, read);
+            }
+            fos.close();
+            out.writeUTF("OK");
+        }catch (Exception e){
+            out.writeUTF("WRONG");
+        }
+    }
+
+    private void disconnected() {
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
