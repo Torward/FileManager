8afaeae98ce017ca5e250e79e23df356a1e7f811
Torward <76395706+Torward@users.noreply.github.com>
2021-05-19 17:16:35 +1000

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,146 @@
+package com.lomov.client;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.*;
+import java.net.Socket;
+
+/**
+ * Swing client - File storage
+ * Client command: upload filename| download filename
+ */
+
+
+public class Client extends JFrame {
+    private final Socket socket;
+    private final DataInputStream in;
+    private final DataOutputStream out;
+
+
+    public Client() throws IOException {
+        //init
+        socket = new Socket("localhost", 6782);
+        out = new DataOutputStream(socket.getOutputStream());
+        in = new DataInputStream(socket.getInputStream());
+
+        //create form
+        setSize(300, 300);
+        JPanel panel = new JPanel(new GridLayout(2, 1));
+        JButton btnSend = new JButton("SEND");
+        JTextField textField = new JTextField();
+        btnSend.addActionListener(a -> {
+            String[] cmd = textField.getText().split(" ");
+            if("upload".equals(cmd[0])){
+                sendFile(cmd[1]);
+//            } else if ("download".equals(cmd[0])){
+//                getFile(cmd[1]);
+//            }
+
+        });
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                sendMessage("exit");
+            }
+        });
+
+        panel.add(textField);
+        panel.add(btnSend);
+        add(panel);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+
+
+    }
+
+//    private void getFile(String filename) {
+//        try{
+//            File file = new File("server/" + filename);
+//            if(!file.exists()){
+//                throw new FileNotFoundException();
+//            }
+//            long fileLength = file.length();
+//            FileInputStream fis = new FileInputStream(file);
+//
+//            out.writeUTF("download");
+//            out.writeUTF(filename);
+//            out.writeLong(fileLength);
+//
+//            int read =0;
+//            byte[] buffer = new byte[8 * 1024];
+//            while ((read = fis.read(buffer)) != -1) {
+//                out.write(buffer, 0, read);
+//
+//            }
+//            out.flush();
+//            String status = in.readUTF();
+//            System.out.println("Sending status: " + status);
+//        } catch (FileNotFoundException e) {
+//            System.err.println("File not found - /server/" + filename);
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//    }
+
+    private void sendFile(String filename) {
+        try {
+            File file = new File("client/" + filename);
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
+
+            long fileLength = file.length();
+            FileInputStream fis = new FileInputStream(file);
+
+            out.writeUTF("upload");
+            out.writeUTF(filename);
+            out.writeLong(fileLength);
+
+            int read = 0;
+            byte[] buffer = new byte[8 * 1024];
+            while ((read = fis.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+
+            }
+            out.flush();
+            String status = in.readUTF();
+            System.out.println("Sending status: " + status);
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found - /client/" + filename);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * message sending
+     *
+     * @param message String
+     */
+    private void sendMessage(String message) {
+        try {
+            out.writeUTF(message);
+            String command = in.readUTF();
+//            if("done".equalsIgnoreCase(command)) {
+//
+//        }
+            System.out.println(command);
+        } catch (EOFException eofException) {
+            System.err.println("Reading command error from" + socket.getInetAddress());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Client();
+    }
+}
